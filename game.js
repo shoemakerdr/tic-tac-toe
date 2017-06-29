7f79a48b67d8c746700db0b992053eae3433f608
Derek Shoemaker <shoemakerdr@gmail.com>
2017-06-29 21:29:42 +0000

    Added constructor and restart and turn methods to Game.

--- game.js
+++ game.js
@@ -1,19 +1,20 @@
+'use strict';
 
-function Game() {
-    return {
-        start: function() {
-            
-        },
-        turn: function() {
-            
-        },
-        isGameOver: function () {
-            return true;
-        },
-        winner: function () {
-            return 'x';
-        }
-    };
+function Game(board) {
+    this.board = board;
+    this.currentPlayer = 'x';
 }
+
+Game.prototype.turn = function (row, column) {
+    this.board.addMove(this.currentPlayer, row, column);
+    this.currentPlayer = (this.currentPlayer === 'x')
+                          ? 'o'
+                          : 'x';
+};
+
+Game.prototype.restart = function () {
+    this.board.resetBoard();
+    this.currentPlayer = 'x';
+};
 
 module.exports = Game;
