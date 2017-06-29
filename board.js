7a2bc0b2a848992523a588c8cc9d000a4c5738cc
Derek Shoemaker <shoemakerdr@gmail.com>
2017-06-29 15:22:52 +0000

    Finished board module.

--- board.js
+++ board.js
@@ -1,3 +1,5 @@
+'use strict';
+
 
 function Board() {
     this.spaces = [
@@ -8,11 +10,7 @@ function Board() {
 }
 
 Board.prototype.resetBoard = function() {
-    this.spaces = [
-        null,null,null,
-        null,null,null,
-        null,null,null
-    ];
+    this.spaces = this.spaces.map(space => null);
 };
 
 Board.prototype.addMove = function (player, index) {
@@ -28,6 +26,47 @@ Board.prototype.isFull = function() {
 
 Board.prototype.isSpaceOccupied = function (index) {
     return (this.spaces[index] !== null);
+};
+Board.prototype.isWinner = function(player) {
+    const spaces = this.spaces;
+    return rowWin(player) || columnWin(player) || diagonalWin(player);
+    
+    function rowWin(player) {
+        const rows = 3;
+        for (let row = 0, column = 0; row < rows; row++) {
+            if (spaces[column] === player 
+                && spaces[column + 1] === player 
+                && spaces[column + 2] === player)
+                return true;
+            column += 3;
+        }
+        return false;
+    }
+    
+    function columnWin(player) {
+        const columns = 3;
+        
+        for (let column = 0, row = 0; column < columns; column++) {
+            if (spaces[row] === player 
+                && spaces[row + 3] === player 
+                && spaces[row + 6] === player)
+                return true;
+            row++;
+        }
+        return false;
+    }
+    
+    function diagonalWin(player) {
+        const leftToRight = spaces[0] === player 
+                            && spaces[4] === player 
+                            && spaces[8] === player;
+        const rightToLeft = spaces[2] === player 
+                            && spaces[4] === player 
+                            && spaces[6] === player;
+        if (leftToRight || rightToLeft)
+            return true;
+        return false;
+    }
 };
 
 module.exports = Board;
--- test/board.test.js
+++ test/board.test.js
@@ -5,6 +5,7 @@ const Board = require('../board');
 describe('Board', function() {
     
     const board = new Board();
+    
     beforeEach(function () {
         board.resetBoard();
     });
@@ -17,10 +18,12 @@ describe('Board', function() {
                 null,null,null
             ]);
         });
-        describe('isFull method', function () {
-            it('should return false', function () {
-                expect(board.isFull()).to.be.false;
-            });
+        it('should know that it is not full', function () {
+            expect(board.isFull()).to.be.false;
+        });
+        it('should not have a winner', function () {
+            expect(board.isWinner('x')).to.be.false;
+            expect(board.isWinner('o')).to.be.false;
         });
     });
     describe('when full', function () {
@@ -35,10 +38,22 @@ describe('Board', function() {
             board.addMove('o', 7);
             board.addMove('x', 8);
         });
-        describe('isFull method', function () {
-            it('should return true', function () {
-                expect(board.isFull()).to.be.true;
-            });
+        it('should know that it is full', function () {
+            expect(board.isFull()).to.be.true;
+        });
+        it('should be able to tell if a particular player is a winner', function () {
+            expect(board.isWinner('x')).to.be.true;
+        });
+        it('should be able to tell if a particular player is NOT a winner', function () {
+            expect(board.isWinner('o')).to.be.false;
+        });
+        it('should be empty after reset', function () {
+            board.resetBoard();
+            expect(board.spaces).to.have.members([
+                null,null,null,
+                null,null,null,
+                null,null,null
+            ]);
         });
     });
     describe('if a particular space is occupied', function () {
@@ -52,9 +67,105 @@ describe('Board', function() {
             expect(function () {board.addMove('x', 0);}).to.throw();
         });
     });
-    describe('if a particular space is occupied', function () {
+    describe('if a particular space is unoccupied', function () {
         it('should know that it is not occupied', function() {
             expect(board.isSpaceOccupied(0)).to.be.false;
+        });
+    });
+    describe('when there is a winning board', function() {
+        describe('with a row win', function () {
+            beforeEach(function () {
+                board.addMove('x', 3);
+                board.addMove('o', 1);
+                board.addMove('x', 4);
+                board.addMove('o', 8);
+                board.addMove('x', 5);
+            });
+            it('should be able to tell if the winning player is the winner', function () {
+                expect(board.isWinner('x')).to.be.true;
+            });
+            it('should be able to tell if the losing player is NOT the winner', function () {
+                expect(board.isWinner('o')).to.be.false;
+            });
+            it('should be empty after reset', function () {
+                board.resetBoard();
+                expect(board.spaces).to.have.members([
+                    null,null,null,
+                    null,null,null,
+                    null,null,null
+                ]);
+            });
+        });
+        describe('with a column win', function () {
+            beforeEach(function () {
+                board.addMove('x', 1);
+                board.addMove('o', 0);
+                board.addMove('x', 4);
+                board.addMove('o', 3);
+                board.addMove('x', 7);
+            });
+            it('should be able to tell if the winning player is the winner', function () {
+                expect(board.isWinner('x')).to.be.true;
+            });
+            it('should be able to tell if the losing player is NOT the winner', function () {
+                expect(board.isWinner('o')).to.be.false;
+            });
+            it('should be empty after reset', function () {
+                board.resetBoard();
+                expect(board.spaces).to.have.members([
+                    null,null,null,
+                    null,null,null,
+                    null,null,null
+                ]);
+            });
+        });
+        describe('with a diagonal win', function () {
+            beforeEach(function () {
+                board.addMove('x', 0);
+                board.addMove('o', 1);
+                board.addMove('x', 4);
+                board.addMove('o', 2);
+                board.addMove('x', 8);
+            });
+            it('should be able to tell if the winning player is the winner', function () {
+                expect(board.isWinner('x')).to.be.true;
+            });
+            it('should be able to tell if the losing player is NOT the winner', function () {
+                expect(board.isWinner('o')).to.be.false;
+            });
+            it('should be empty after reset', function () {
+                board.resetBoard();
+                expect(board.spaces).to.have.members([
+                    null,null,null,
+                    null,null,null,
+                    null,null,null
+                ]);
+            });
+        });
+    });
+    describe('when a full board is a draw', function () {
+        beforeEach(function () {
+            board.addMove('x', 0);
+            board.addMove('o', 4);
+            board.addMove('x', 8);
+            board.addMove('o', 3);
+            board.addMove('x', 5);
+            board.addMove('o', 2);
+            board.addMove('x', 6);
+            board.addMove('o', 7);
+            board.addMove('x', 1);
+        });
+        it('should have no winner', function () {
+            expect(board.isWinner('x')).to.be.false;
+            expect(board.isWinner('o')).to.be.false;
+        });
+        it('should be empty after reset', function () {
+            board.resetBoard();
+            expect(board.spaces).to.have.members([
+                null,null,null,
+                null,null,null,
+                null,null,null
+            ]);
         });
     });
 });
