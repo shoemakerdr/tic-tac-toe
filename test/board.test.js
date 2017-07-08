9bf680ff9e2d97509725c27105e0abf391a0a355
Derek Shoemaker <shoemakerdr@gmail.com>
2017-07-07 20:29:01 -0500

    Removed tests for winning.

--- test/board.test.js
+++ test/board.test.js
@@ -10,23 +10,31 @@ function expectEmptyBoard() {
     }
 }
 
+function expectedAvailableSpaces(expectedArray) {
+    const len = expectedArray.length;
+    for (let pair = 0; pair < len; pair++) {
+        expect(board.availableSpaces()[pair]).to.have.ordered.members(expectedArray[pair]);
+    }
+}
+
 describe('Board', function() {
-    
     beforeEach(function () {
         board.resetBoard();
     });
-    
     describe('when empty', function () {
         it('should return all spaces as null', function () {
-            expectEmptyBoard();    
-                
+            expectEmptyBoard();
+
         });
         it('should know that it is not full', function () {
             expect(board.isFull()).to.be.false;
         });
-        it('should not have a winner', function () {
-            expect(board.isWinner('x')).to.be.false;
-            expect(board.isWinner('o')).to.be.false;
+        it('should show that all spaces are available', function () {
+            expectedAvailableSpaces([
+                [0,0],[0,1],[0,2],
+                [1,0],[1,1],[1,2],
+                [2,0],[2,1],[2,2]
+            ]);
         });
     });
     describe('when full', function () {
@@ -44,20 +52,17 @@ describe('Board', function() {
         it('should know that it is full', function () {
             expect(board.isFull()).to.be.true;
         });
-        it('should be able to tell if a particular player is a winner', function () {
-            expect(board.isWinner('x')).to.be.true;
-        });
-        it('should be able to tell if a particular player is NOT a winner', function () {
-            expect(board.isWinner('o')).to.be.false;
-        });
         it('should be empty after reset', function () {
             board.resetBoard();
             expectEmptyBoard();
         });
+        it('should show that no spaces are available', function () {
+            expectedAvailableSpaces([]);
+        })
     });
     describe('if a particular space is occupied', function () {
         beforeEach(function () {
-            board.addMove('x', 0, 0); 
+            board.addMove('x', 0, 0);
         });
         it('should know that it is occupied', function() {
             expect(board.isSpaceOccupied(0, 0)).to.be.true;
@@ -71,65 +76,6 @@ describe('Board', function() {
             expect(board.isSpaceOccupied(0, 0)).to.be.false;
         });
     });
-    describe('when there is a winning board', function() {
-        describe('with a row win', function () {
-            beforeEach(function () {
-                board.addMove('x', 1, 0);
-                board.addMove('o', 0, 1);
-                board.addMove('x', 1, 1);
-                board.addMove('o', 2, 2);
-                board.addMove('x', 1, 2);
-            });
-            it('should be able to tell if the winning player is the winner', function () {
-                expect(board.isWinner('x')).to.be.true;
-            });
-            it('should be able to tell if the losing player is NOT the winner', function () {
-                expect(board.isWinner('o')).to.be.false;
-            });
-            it('should be empty after reset', function () {
-                board.resetBoard();
-                expectEmptyBoard();
-            });
-        });
-        describe('with a column win', function () {
-            beforeEach(function () {
-                board.addMove('x', 0, 1);
-                board.addMove('o', 0, 0);
-                board.addMove('x', 1, 1);
-                board.addMove('o', 1, 0);
-                board.addMove('x', 2, 1);
-            });
-            it('should be able to tell if the winning player is the winner', function () {
-                expect(board.isWinner('x')).to.be.true;
-            });
-            it('should be able to tell if the losing player is NOT the winner', function () {
-                expect(board.isWinner('o')).to.be.false;
-            });
-            it('should be empty after reset', function () {
-                board.resetBoard();
-                expectEmptyBoard();
-            });
-        });
-        describe('with a diagonal win', function () {
-            beforeEach(function () {
-                board.addMove('x', 0, 0);
-                board.addMove('o', 0, 1);
-                board.addMove('x', 1, 1);
-                board.addMove('o', 0, 2);
-                board.addMove('x', 2, 2);
-            });
-            it('should be able to tell if the winning player is the winner', function () {
-                expect(board.isWinner('x')).to.be.true;
-            });
-            it('should be able to tell if the losing player is NOT the winner', function () {
-                expect(board.isWinner('o')).to.be.false;
-            });
-            it('should be empty after reset', function () {
-                board.resetBoard();
-                expectEmptyBoard();
-            });
-        });
-    });
     describe('when a full board is a draw', function () {
         beforeEach(function () {
             board.addMove('x', 0, 0);
@@ -141,10 +87,6 @@ describe('Board', function() {
             board.addMove('x', 2, 0);
             board.addMove('o', 2, 1);
             board.addMove('x', 0, 1);
-        });
-        it('should have no winner', function () {
-            expect(board.isWinner('x')).to.be.false;
-            expect(board.isWinner('o')).to.be.false;
         });
         it('should be empty after reset', function () {
             board.resetBoard();
