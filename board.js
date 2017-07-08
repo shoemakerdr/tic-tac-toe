eb89b61cc05f4a044f7328602b67ac60b7e01643
Derek Shoemaker <shoemakerdr@gmail.com>
2017-07-07 20:28:36 -0500

    Removed isWinner method.

--- board.js
+++ board.js
@@ -32,48 +32,19 @@ Board.prototype.isSpaceOccupied = function (row, column) {
     return (this.spaces[row][column] !== null);
 };
 
-Board.prototype.isWinner = function(player) {
+Board.prototype.availableSpaces = function () {
     const spaces = this.spaces;
-    return rowWin(player) || columnWin(player) || diagonalWin(player);
-    
-    function rowWin(player) {
-        const rows = 3;
-        for (let row = 0; row < rows; row++) {
-            if (
-                spaces[row][0] === player 
-                && spaces[row][1] === player 
-                && spaces[row][2] === player
-            )
-                return true;
-        }
-        return false;
-    }
-    
-    function columnWin(player) {
-        const columns = 3;
-        
+    let available = [];
+    const rows = 3;
+    const columns = 3;
+    for (let row = 0; row < rows; row++) {
         for (let column = 0; column < columns; column++) {
-            if (
-                spaces[0][column] === player 
-                && spaces[1][column] === player 
-                && spaces[2][column] === player
-            )
-                return true;
+            if (spaces[row][column] === null) {
+                available.push([row,column]);
+            }
         }
-        return false;
     }
-    
-    function diagonalWin(player) {
-        const leftToRight = spaces[0][0] === player 
-                            && spaces[1][1] === player 
-                            && spaces[2][2] === player;
-        const rightToLeft = spaces[0][2] === player 
-                            && spaces[1][1] === player 
-                            && spaces[2][0] === player;
-        if (leftToRight || rightToLeft)
-            return true;
-        return false;
-    }
+    return available;
 };
 
 module.exports = Board;
